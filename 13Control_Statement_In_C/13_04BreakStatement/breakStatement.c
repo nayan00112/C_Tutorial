7baf967ccabb44fc78f4f0f46350a1677f0ad818
Nayan Patel <nayanpatel00112@gmail.com>
2022-05-23 22:38:02 +0530

    C Language

--- 13Control_Statement_In_C/13_04BreakStatement/breakStatement.c
+++ 13Control_Statement_In_C/13_04BreakStatement/breakStatement.c
@@ -7,4 +7,56 @@ The break statement break statement breaks the loop one by one, i.e. in the case
 The break statement in C can be used in the following two scenarios:
     1. With swich case
     2. With loop
+
 */
+
+#include <stdio.h>
+void main()
+{
+    // With loop:
+    int i;
+    for (i = 0; i <= 5; i++)
+    {
+        if (i == 3)
+        {
+            break;
+        }
+        else
+        {
+            printf("%d\n", i);
+        }
+    }
+    // Output:
+    // 0
+    // 1
+    // 2
+    printf("Outside the loop, value of i id: %d", i);
+    // Outside the loop, value of i id: 3
+
+    //----------------------------------------------------------------------
+
+    // With swich case:
+    int j = 2;
+    switch (j)
+    {
+    case 1:
+        printf("\nOne");
+        break;
+    case 2:
+        printf("\nTwo");
+        break;
+    case 3:
+        printf("\nThree");
+        break;
+    case 4:
+        printf("\nFour");
+        break;
+    case 5:
+        printf("\nFive");
+        break;
+    default:
+        printf("\ndefault");
+        break;
+    }
+    // Output: Two
+}
---
+++ 13Control_Statement_In_C/13_05ContinueStatement/continueSatatement.c
@@ -0,0 +1,32 @@
+/*
+Continue Statement in C:
+    The continue statement in C lanuage is used to bring the program control to the beginning of the loop.
+    The continue statement skips some lines of code inside the loop and continues with the next iteration.
+    It is mainly used for a condition so thet we can skip some code for a particular condition.
+*/
+#include <stdio.h>
+void main()
+{
+    int num = 5;
+    int i;
+    for (i = -5; i <= 5; i++)
+    {
+        if (i != 0)
+        printf("\n The %d / %i = %d (int value)", num, i, num / i);
+        else //  num / i -> 5 / 0 -> error so it is continue.
+        {
+            continue;
+        }
+    }
+    // Output:
+    // The 5 / -5 = -1 (int value)
+    // The 5 / -4 = -1 (int value)
+    // The 5 / -3 = -1 (int value)
+    // The 5 / -2 = -2 (int value)
+    // The 5 / -1 = -5 (int value)
+    // The 5 / 1 = 5 (int value)
+    // The 5 / 2 = 2 (int value)
+    // The 5 / 3 = 1 (int value)
+    // The 5 / 4 = 1 (int value)
+    // The 5 / 5 = 1 (int value)
+}
