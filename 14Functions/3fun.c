58a2abf223f0a66a991e96a12f40380e7ec79304
Nayan Patel <nayanpatel00112@gmail.com>
2022-05-24 21:45:33 +0530

    C Language

---
+++ 14Functions/3fun.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void ElectionValidation();
+
+void main()
+{
+    int age;
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    ElectionValidation(age);
+    
+}
+
+void ElectionValidation(int age){
+    if (age > 18){
+        printf("You can voteing.\n");
+    }
+    else{
+        printf("You can not voteing now.\n");
+    }
+}
+/*
+Output:
+1.
+Enter your age: 23
+You can voteing.
+
+2.
+Enter your age: 2
+You can not voteing now.
+*/
