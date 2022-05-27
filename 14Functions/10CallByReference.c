e2da0603c6befec1e99b50c6c6d7a88054db60c8
Nayan Patel <nayanpatel00112@gmail.com>
2022-05-27 20:53:32 +0530

    C Language

--- 14Functions/10CallByReference.c
+++ 14Functions/10CallByReference.c
@@ -1,18 +1,22 @@
 // Call by Referance
 
 #include <stdio.h>
-void Changex(int);
-void main(){
+void Changex(int *);
+void main()
+{
     int x;
     printf("You learn more about in pointer topic.\n");
     x = 6;
     printf("The value of x is %d\n", x);
     Changex(&x);
     printf("After calling Changex function is %d\n", x);
-
-    
 }
 
-void Changex(int x){
-    *x = 1; 
+void Changex(int *x)
+{
+    *x = 3;
 }
+
+// Output:
+// The value of x is 6
+// After calling Changex function is 3
