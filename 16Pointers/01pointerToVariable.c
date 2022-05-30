3aa773ea8085f39add181da8fe25c7c3ca1a954e
Nayan Patel <nayanpatel00112@gmail.com>
2022-05-30 23:27:08 +0530

    C Language

--- 16Pointers/01pointerToVariable.c
+++ 16Pointers/01pointerToVariable.c
@@ -7,12 +7,14 @@ void main()
     p = &a;
 
     printf("The value of a is %d.\n", a);
-    printf("The value address of pointer p is %x.\n", p);
+    printf("The value address of pointer p is %x.\n", p); // %x -> unsigned hexadecimal
+    printf("The value address of pointer p is %p.\n", p); // %x -> unsigned hexadecimal
     printf("The value of pointer p is %d.\n", *p);
     printf("The value of p is %d.\n", p);
     // Output:
     // The value of a is 4.
-    // The value address of pointer p is 2f3ff904.
+    // The value address of pointer p is 233ff844.
+    // The value address of pointer p is 00000019233ff844.
     // The value of pointer p is 4.
-    // The value of p is 792721668.
+    // The value of p is 591394884.
 }
