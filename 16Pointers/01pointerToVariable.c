7cecd686304e2638ebaffe448bc70bcbc5139254
Nayan Patel <nayanpatel00112@gmail.com>
2022-05-30 23:28:14 +0530

    C Language

--- 16Pointers/01pointerToVariable.c
+++ 16Pointers/01pointerToVariable.c
@@ -17,4 +17,18 @@ void main()
     // The value address of pointer p is 00000019233ff844.
     // The value of pointer p is 4.
     // The value of p is 591394884.
+
+    /*
+    Note that the * sign can be confusing here, as it does two different things in our code:
+
+    When used in declaration (int* ptr), it creates a pointer variable.
+    When not used in declaration, it act as a dereference operator.
+    Why Should I Learn About Pointers? Pointers are important in C, because they give you the ability to manipulate the data in the computer's memory - this can reduce the code and improve the performance.
+
+    Good To Know: There are three ways to declare pointer variables, but the first way is mostly used:
+
+        int* myNum; // Most used
+        int *myNum;
+        int * myNum;
+    */
 }
