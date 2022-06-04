93df54b948724b17e451382967a5a552bbad0141
Nayan Patel <nayanpatel00112@gmail.com>
2022-06-04 08:26:50 +0530

    C Language

--- 16Pointers/07ArithmeticPointer.c
+++ 16Pointers/07ArithmeticPointer.c
@@ -1,5 +1,15 @@
-#include<stdio.h>
-void main(){
+#include <stdio.h>
+void main()
+{
+
+    /*
+    Following operations are possible on the pointer in C language.
+        > Increment
+        > Decrement
+        > Addition
+        > Subtarction
+        > Comparision
+    */
     int a;
     int *p = &a;
     printf("The value of pointer p is : %u\n", p);
@@ -7,8 +17,33 @@ void main(){
     printf("Now, the value of pointer p is : %u\n", p);
     p = p + 1;
     printf("Now, the value of pointer p is : %u\n", p);
-// Output:
-// The value of pointer p is : 1583349220
-// Now, the value of pointer p is : 1583349216
-// Now, the value of pointer p is : 1583349220
+    p++;
+    printf("Now, the value of pointer p is : %u\n", p);
+    p--;
+    printf("Now, the value of pointer p is : %u\n", p);
+    --p;
+    printf("Now, the value of pointer p is : %u\n", p);
+    ++p;
+    printf("Now, the value of pointer p is : %u\n", p);
+
+    // Output:
+    // The value of pointer p is : 3735026260
+    // Now, the value of pointer p is : 3735026256
+    // Now, the value of pointer p is : 3735026260
+    // Now, the value of pointer p is : 3735026264
+    // Now, the value of pointer p is : 3735026260
+    // Now, the value of pointer p is : 3735026256
+    // Now, the value of pointer p is : 3735026260
 }
+
+/*
+ILLEGAL arithmatic with pointer.
+    > add + add = illegal
+    > add * add = illegal
+    > add % add = illegal
+    > add / add = illegal
+    > add & add = illegal
+    > add ^ add = illegal
+    > add | add = illegal
+    > ~add = illegal
+*/
