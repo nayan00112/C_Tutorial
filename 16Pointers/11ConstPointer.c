e89a0772431f6fcd9ebefc156138ca0c37ceeaec
Nayan Patel <nayanpatel00112@gmail.com>
2022-06-04 10:31:06 +0530

    C Language

--- 16Pointers/11ConstPointer.c
+++ 16Pointers/11ConstPointer.c
@@ -3,13 +3,15 @@
 A constant pointer in C cannot change the address of the variable to which it is pointing, 
 i.e. the address will remaain constant.
 Therefore, we can say that if a constant pointer is pointing to some variable, then it cannot point to any other variable.
+
+https://www.javatpoint.com/const-pointer-in-c
 */
 
 #include<stdio.h>
 void main(){
     char ch = 'n';
     char *vp = &ch;
-    char *const p = &ch;
+    char *const p = &ch; // const
 
     printf("%d\n", sizeof(ch)); // 1
     printf("%d\n", sizeof(vp)); // 8
