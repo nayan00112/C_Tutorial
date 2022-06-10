a9817d60e8592aff28c745a0e2a93493b099770c
Nayan Patel <nayanpatel00112@gmail.com>
2022-06-10 20:58:15 +0530

    C Language

--- 17Dynamic_memory_allocation/01malloc.c
+++ 17Dynamic_memory_allocation/01malloc.c
@@ -6,7 +6,7 @@ int main()
     int n, i, *ptr, sum = 0;
     printf("Enter number of elements: ");
     scanf("%d", &n);
-    ptr = (int *)calloc(n, sizeof(int)); // memory allocated using malloc
+    ptr = (int *)malloc(n * sizeof(int)); // memory allocated using malloc
     if (ptr == NULL)
     {
         printf("Sorry! unable t23o allocate memory");
