d53b831fac81133dc25017e47cc170e985b34d3e
Nayan Patel <nayanpatel00112@gmail.com>
2022-07-03 20:14:19 +0530

    C Language

--- 18String/11strlen_strings_function.c
+++ 18String/11strlen_strings_function.c
@@ -1,14 +1,14 @@
 // https://www.javatpoint.com/c-string-functions
 
+// strlen for find length of string.
 #include<stdio.h>
 #include<string.h>
 
 void main()
 {
     char name[] = "Nayan Patel";
-
+    char add[] = "Nayan";
     printf("the length of the string is the %d.", strlen(name));
+    printf("add %d.", strlen(add));
     // the length of the string is the 11.
-
-    
 }
