071c049da9e1911c3399b358d1039e1a16c3cad7
Nayan Patel <nayanpatel00112@gmail.com>
2022-07-06 19:55:24 +0530

    C Language

---
+++ 20Structure&Union/11Union.c
@@ -0,0 +1,46 @@
+// https://www.javatpoint.com/c-union
+
+// Union
+
+#include <stdio.h>
+#include<string.h>
+struct student
+{
+    char name[20];
+    union dateOfBirth
+    {
+        int dd;
+        int mm;
+        int yyyy;
+    } dob;
+
+    int rolnum;
+    union address
+    {
+        char city[10];
+        int pincode;
+    } addr;
+
+    char phoneNo[14];
+};
+
+int main()
+{
+    struct student s1;
+    //s1.name = "Nayan"; //Wrong Way, True way given below:
+    strcpy(s1.name, "Nayan");
+
+    s1.rolnum = 87;
+    //s1.phoneNo = "9876"; //Wrong Way, True way given below:
+    strcpy(s1.phoneNo, "98776");
+    s1.dob.dd = 1;
+    s1.dob.mm = 2;
+    s1.dob.yyyy = 3;
+    //s1.addr.city = "Anjar"; //Wrong Way, True way given below:
+    strcpy(s1.addr.city, "Anjar");
+    s1.addr.pincode = 3701;
+
+    printf("%u", sizeof(s1)); //56
+    return 0;
+}
+
